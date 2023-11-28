61441d7de4909277866158e28b105b74edd00a8e
Tam <Tam@TAM-DESKTOP>
2023-11-28 17:10:40 +0700

    Cài đặt CORS

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 import express, {Express, Request, Response} from "express";
 import dotenv from "dotenv"; 
+import cors from "cors";
 import * as database from "./config/database"; 
 import mainV1Route from "./api/v1/routes/index.route";
-import bodyParser = require("body-parser");
+import bodyParser from "body-parser";
 
 dotenv.config();
 database.connect();
 
 const app: Express = express();
 const port:number|string = process.env.PORT || 3000;
+
+// const corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 // phải để trước mainV1Route mới kích hoạt dọc body cho các route
 app.use(express.json());
