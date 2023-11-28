422c1e7e335a6c2cd0d6d001d8c0fc38b2eef62a
Tam <Tam@TAM-DESKTOP>
2023-11-28 17:41:55 +0700

    Tính năng đăng ký

--- api/v1/routes/index.route.ts
+++ api/v1/routes/index.route.ts
@@ -1,9 +1,12 @@
 import { taskRoutes } from "./task.route";
 import {Express} from "express";
+import { userRoutes } from "./user.route";
 const mainV1Route = (app: Express) =>{
     const version: string = "/api/v1";
 
     app.use(version + "/tasks", taskRoutes);
+
+    app.use(`${version}/users`, userRoutes);
 }
 
 export default mainV1Route;
